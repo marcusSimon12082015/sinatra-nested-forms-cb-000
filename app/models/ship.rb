7d34704f293102026d39cb39b75fed5630c1f25e
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-23 15:35:01 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,12 @@
 class Ship
+  attr_accessor :name,:type,:booty
+  @@all = []
+  def initialize(name,type,booty)
+    @name = name
+    @type = type
+    @booty = booty
+  end
+  def self.all
+    @@all
+  end
 end
