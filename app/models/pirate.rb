60b983fcee2c3d35802e8e6cecd9d75ca434c20e
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-23 16:15:24 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,14 @@
 class Pirate
+  @@all = []
+  attr_accessor :name, :weight, :height
+
+  def initialize(name,weight,height)
+    @name = name
+    @weight = weight
+    @height = height
+  end
+
+  def self.all
+    @@all
+  end
 end
